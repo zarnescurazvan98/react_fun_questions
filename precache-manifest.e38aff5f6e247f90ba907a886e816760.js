d73535bda3b7986a48392f58b081dd51ac086efd
Razvan Zarnescu <zarnescurazvan98@gmail.com>
2021-09-27 21:37:14 +0300

    Updates

---
+++ precache-manifest.e38aff5f6e247f90ba907a886e816760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "210c5f1a3cd81612c80a5abd967e2053",
+    "url": "/react_fun_questions/index.html"
+  },
+  {
+    "revision": "c1e532577b8385903aa1",
+    "url": "/react_fun_questions/static/css/main.789496d0.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/react_fun_questions/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_fun_questions/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e532577b8385903aa1",
+    "url": "/react_fun_questions/static/js/main.1c6ff7ba.chunk.js"
+  },
+  {
+    "revision": "ae3cb18c86d910e8a0a2",
+    "url": "/react_fun_questions/static/js/runtime-main.46c3831e.js"
+  }
+]);
